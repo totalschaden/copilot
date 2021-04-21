288dab7226f962b94b4233ed1ac4db01d260d0ed
Totalschaden <autotuner@arcor.de>
2021-04-21 16:47:17 +0200

    Check if Skill can be used Changes for getting enemys

--- CoPilot.cs
+++ CoPilot.cs
@@ -482,7 +482,7 @@ namespace CoPilot
                     vaalSkills = localPlayer.GetComponent<Actor>().ActorVaalSkills;
                     playerPosition = GameController.Player.Pos;
 
-                    enemys = GameController.Entities.Where(x => x != null && x.IsValid && x.IsAlive && x.IsHostile && !x.IsHidden && x.IsTargetable && x.HasComponent<Monster>() && x.HasComponent<Life>() && x.GetComponent<Life>().CurHP > 0 && !HasStat(x,GameStat.CannotBeDamaged) &&
+                    enemys = GameController.EntityListWrapper.ValidEntitiesByType[EntityType.Monster].Where(x => x != null && x.IsAlive && x.IsHostile &&  x.HasComponent<Targetable>() && x.GetComponent<Targetable>().isTargetable && x.HasComponent<Life>() && x.GetComponent<Life>().CurHP > 0 && !HasStat(x, GameStat.CannotBeDamaged) &&
                     GameController.Window.GetWindowRectangleTimeCache.Contains(GameController.Game.IngameState.Camera.WorldToScreen(x.Pos))).ToList();
                     if (Settings.offeringsEnabled || Settings.autoZombieEnabled)
                         corpses = GameController.Entities.Where(x => x.IsValid && !x.IsHidden && x.IsHostile && x.IsDead && x.IsTargetable && x.HasComponent<Monster>()).ToList();
@@ -548,7 +548,7 @@ namespace CoPilot
                     // Currently thats unanavailable in API.
                     foreach (var skill in skills)
                     {
-                        if (!skill.IsOnSkillBar || skill.SkillSlotIndex < 1 || skill.SkillSlotIndex == 2)
+                        if (!skill.IsOnSkillBar || skill.SkillSlotIndex < 1 || skill.SkillSlotIndex == 2 || !skill.CanBeUsed)
                             continue;
 
                         #region Mirage Archer
