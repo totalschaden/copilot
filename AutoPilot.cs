f0480ce4e84bd4f224fe98eeb525b0e5dd986f9e
Totalschaden <autotuner@arcor.de>
2021-08-14 18:14:23 +0200

    Remove Namespace

--- AutoPilot.cs
+++ AutoPilot.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Net.Mime;
 using ExileCore;
 using ExileCore.PoEMemory.Components;
 using ExileCore.PoEMemory.MemoryObjects;
